c91d9d6102d49392e3dc4a6ab17ecfec836780d8
tcrurup <tcrurup@gmail.com>
2020-07-08 18:26:44 -0600

    state updates to form data on change and clears field on submit

--- table-top-frontend/src/components/homePage/LoginInput.js
+++ table-top-frontend/src/components/homePage/LoginInput.js
@@ -1,18 +1,44 @@
-import React, { component, Component } from 'react'
+import React, { Component } from 'react'
 
 class LoginInput extends Component{
 
     constructor(props){
         super(props)
         this.state = {
-
+            username: '',
+            password: ''
         }
+    }
+
+    handleChange = event => {
+        event.preventDefault()
+        console.log(`setting ${event.target.id} state to ${event.target.value}`)
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        const formData = {...this.state}
+        this.setState({
+            username: '',
+            password: ''
+        })
     }
 
     render(){
         return (
             <div id='login-form'>
-                <form>
+                <h3>Welcome To Table Top!</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='username'>Username: </label>
+                    <input type='text' id='username' onChange={this.handleChange} value={this.state.username}/>
+                    <p/>
+                    <label htmlFor='password'>Password: </label>
+                    <input type='password' id='password' onChange={this.handleChange} value={this.state.password}/>
+                    <p/>
+                    <input type='submit' value='Login' />
                 </form>
             </div>
         )
