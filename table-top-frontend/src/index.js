f65290f18feef8d179591bfecd3275680ddb3c31
tcrurup <tcrurup@gmail.com>
2020-07-08 19:10:52 -0600

    create user reducer, create store and provide store to rest of application

--- table-top-frontend/src/index.js
+++ table-top-frontend/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
+import { createStore } from 'redux'
+import reducer from './reducers/index.js'
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  
+)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ table-top-frontend/src/reducers/userReducer.js
@@ -0,0 +1,9 @@
+
+export default function userReducer( state = {}, action){
+
+    switch(action.type){
+
+        default:
+            return state
+    }
+}
