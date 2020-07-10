7f7d69ed27742eee634cd54279c1ff75d2106902
tcrurup <tcrurup@gmail.com>
2020-07-10 08:08:58 -0600

    user can toggle log in to be a signup sheet also

--- table-top-frontend/src/components/homePage/LoginInput.js
+++ table-top-frontend/src/components/homePage/LoginInput.js
@@ -6,11 +6,32 @@ class LoginInput extends Component{
         super(props)
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            email: '',
+            type: 'LOGIN'
         }
     }
 
-    clearFields = () => this.setState({ username: '', password: ''})
+    clearFields = () => this.setState({ username: '', password: '', email: ''})
+
+    createEmailField = () => {
+        if(this.state.type === 'SIGNUP'){
+            return (
+                <>
+                    <label htmlFor='email'>Email: </label>
+                    <input type='email' id='email' onChange={this.handleChange} value={this.state.email}/>
+                </>
+            )
+        }
+    }
+
+    createSubtext = () => {
+        if(this.state.type === 'SIGNUP'){
+            return <h5>Already a member? <a href='' onClick={this.toggleFormType}>Log in</a> </h5>
+        } else {
+            return <h5>Not a member? <a href='' onClick={this.toggleFormType}>Sign up</a> </h5>
+        }    
+    }
 
     handleChange = event => {
         event.preventDefault()
@@ -27,6 +48,14 @@ class LoginInput extends Component{
         this.clearFields()
     }
 
+    toggleFormType = event => {
+        event.preventDefault()
+        let newType
+        this.state.type === 'LOGIN' ? newType = 'SIGNUP' : newType = 'LOGIN'
+        console.log(`setting form type to ${newType}`)
+        this.setState({ type: newType })
+    }
+
     render(){
         return (
             <div id='login-form'>
@@ -38,8 +67,11 @@ class LoginInput extends Component{
                     <label htmlFor='password'>Password: </label>
                     <input type='password' id='password' onChange={this.handleChange} value={this.state.password}/>
                     <p/>
-                    <input type='submit' value='Login' />
+                    <p> {this.createEmailField()} </p>
+                    <input type='submit' value={this.state.type} />
                 </form>
+
+                {this.createSubtext()}
             </div>
         )
     }
