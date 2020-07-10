13221ca0dd579c46e8b3486d9ecebc5ef5581c46
tcrurup <tcrurup@gmail.com>
2020-07-10 11:55:22 -0600

    refactor and remove some unused code

--- table-top-backend/app/controllers/users_controller.rb
+++ table-top-backend/app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
 
     def login
-        type = user_params[:type]
+
+        type = user_params[:type]        
+        
+        
         if type === 'LOGIN'
-            #login user
+            attemptLogin(user_params.except(:type))            
         elsif type == 'SIGNUP'
             User.create( user_params.except(:type) )            
         end
@@ -11,6 +14,25 @@ class UsersController < ApplicationController
     end
 
     private
+
+    def attemptLogin(user_params)        
+        user = User.find_by(username: user_params[:username])
+        password = user_params[:password]
+
+        user ? authenticatePassword(user, password) : onLoginFailure('not found')
+    end
+
+    def authenticatePassword(user, password)
+        user.authenticate(password) ? onLoginSuccess() : onLoginFailure('wrong password')
+    end
+
+    def onLoginSuccess
+        puts 'login success'
+    end
+
+    def onLoginFailure(message = "")
+        puts message
+    end
 
     def user_params
         params.require(:user).permit(:username, :password, :email, :type)
--- table-top-backend/app/models/user.rb
+++ table-top-backend/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     #ASSOCIATIONS
 
     has_secure_password
-    has_secure_token :id_token
+    #has_secure_token :id_token
 
 
 end
