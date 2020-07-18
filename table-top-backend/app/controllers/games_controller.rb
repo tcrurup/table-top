7d1968924d73c320255271e8e3575cf0d76764d0
tcrurup <tcrurup@gmail.com>
2020-07-17 19:37:14 -0600

    fix bug in games controller

--- table-top-backend/app/controllers/games_controller.rb
+++ table-top-backend/app/controllers/games_controller.rb
@@ -1,13 +1,16 @@
 class GamesController < ApplicationController
 
     def load
+
         user = User.find_by(id: game_params[:user_id].to_i)
         game_room = GameRoom.find(game_params[:id].to_i)
         render json: GameSerializer.new(game_room.game).to_serialized_json
     end
     
     def delete
-        puts game_params
+        user = User.find_by(id: game_params[:user_id].to_i)
+        game_room = GameRoom.find(game_params[:id].to_i)
+        game_room.delete_game #if user.has_room?(game_room)
         render json: user.game_rooms
     end
 
