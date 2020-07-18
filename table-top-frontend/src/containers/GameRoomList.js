b1926691adc3cae4fa31cd36aefa08c3cbd70465
tcrurup <tcrurup@gmail.com>
2020-07-17 19:36:51 -0600

    move Redux connection from GameRoomList to HomePage and change into presentation component

--- table-top-frontend/src/containers/GameRoomList.js
+++ table-top-frontend/src/containers/GameRoomList.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { loadGame, createGame, deleteGame, focusCard, flipCardToFront } from '../actions/gameActions.js'
+import React from 'react'
 import GameRoomCard from '../components/GameRoomList/GameRoomCard.js'
 
 
-class GameRoomList extends Component{
+const GameRoomList = props => {
     
-    html = () => 
-        <div className='game-rooms-container'>
-            <h2>Your Games</h2>
-            <div className = 'save-slots-container'>
-                {this._createGameCard()}
-            </div>
-        </div> 
-    
-    
-    render(){return this.html()}
-    
-    //********PRIVATE METHODS********\\
-    
-    _createGameCard(){
-        return this.props.gameRooms.map( game =>
+    const createGameCard = () => {
+        return props.gameRooms.map( game =>
             <GameRoomCard 
                 game={game}
-                user={this.props.user} 
-                focusCard={this.props.focusCard}
-                loadGame={this.props.loadGame}
-                createGame={this.props.createGame}
-                deleteGame={this.props.deleteGame}
-                flipToFront={this.props.flipCardToFront}    
+                user={props.user} 
+                focusCard={props.focusCard}
+                loadGame={props.loadGame}
+                createGame={props.createGame}
+                deleteGame={props.deleteGame}
+                flipToFront={props.flipCardToFront}    
             />
         )
-    }   
+    }
+    
+    return <div className='game-rooms-container'>
+        <h2>Your Games</h2>
+        <div className = 'save-slots-container'>
+            {createGameCard()}
+        </div>
+    </div> 
+
 }
 
 
