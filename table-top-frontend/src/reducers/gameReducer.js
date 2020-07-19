5f51729840f2419c7cecac2c748d3b9b239da82a
tcrurup <tcrurup@gmail.com>
2020-07-18 20:07:29 -0600

    remove unused code

--- table-top-frontend/src/reducers/gameReducer.js
+++ table-top-frontend/src/reducers/gameReducer.js
@@ -1,17 +1,5 @@
 
 export default function gameReducer ( state = {}, action){
-
-    const serialize = body => {
-        return({
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        })
-    }        
-    
     switch(action.type){
 
         case "LOAD_GAME":
