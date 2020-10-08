2f4b4552c966ed4a9a3572b7ededd2d20a3d3b42
tcrurup <tcrurup@gmail.com>
2020-10-08 14:02:00 -0600

    change styling on back of flip card

--- table-top-frontend/src/components/HomePage/GameRoomCard.js
+++ table-top-frontend/src/components/HomePage/GameRoomCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import GameRoomCardBack from './GameRoomCard/GameRoomCardBack'
 import GameRoomCardFront from './GameRoomCard/GameRoomCardFront'
 
@@ -12,36 +12,43 @@ focusCard - function that will enlarge the card with passed game id and minimize
 
 \********PROPS********/
 
-const GameRoomCard = props =>{
+class GameRoomCard extends Component{
 
-    const className = () => `game-rooms-container-card-empty ${props.game_room.focus ? 'flipped' : ''}`
-    
-    const focusCard = () => {
-        console.log(props)
-        if(props.game_room.focus == false){
-            return props.focusCard(props.game_room.id)
+    constructor(props){
+        super(props)
+        this.state = {
+            rating: 0,
+            userRated: false
         }
     }
     
-    return <>
-        <div className={className()} onClick={focusCard}>
+    className = () => `game-rooms-container-card-empty ${this.props.game_room.focus ? 'flipped' : ''}`
+    
+    focusCard = () => {
+        if(this.props.game_room.focus == false){
+            return this.props.focusCard(this.props.game_room.id)
+        }
+    }
+
+    render(){
+        return <div className={this.className()} onClick={this.focusCard}>
             <div className='flip-card'>
                 <div className="flip-card-front">
-                    <GameRoomCardFront game={props.game_room.game} />
+                    <GameRoomCardFront game={this.props.game_room.game} />
                 </div>
                 <div className="flip-card-back">
-                    <button class='minimize' onClick={() => props.flipToFront(props.game_room.id)}>-</button>
+                    <button class='minimize' onClick={() => this.props.flipToFront(this.props.game_room.id)}>-</button>
                     < GameRoomCardBack 
-                        game_room={props.game_room}
-                        user={props.user}
-                        deleteGame = {props.deleteGame} 
-                        loadGame ={props.loadGame}
-                        createGame ={props.createGame}
+                        game_room={this.props.game_room}
+                        user={this.props.user}
+                        deleteGame = {this.props.deleteGame} 
+                        loadGame ={this.props.loadGame}
+                        createGame ={this.props.createGame}
                     />
                 </div>
             </div>
         </div>
-    </>
+    }
 
     
         
--- table-top-frontend/src/components/HomePage/GameRoomCard/GameRoomCardBack/GameRoomThumbnail.js
+++ table-top-frontend/src/components/HomePage/GameRoomCard/GameRoomCardBack/GameRoomThumbnail.js
@@ -3,8 +3,10 @@ import React from 'react'
 const GameRoomThumbnail = props => <>
     {props.game.name}
     
-    <button type='button' onClick={() => props.loadGame(props.game)}>Launch Game</button>
-    <button type='button' onClick={() => props.deleteGame({...props.game, user_id: props.userId})}>Delete Game</button>
+    <div class='button-container'>
+        <button type='button' onClick={() => props.loadGame(props.game)}>Launch</button>
+        <button type='button' onClick={() => props.deleteGame({...props.game, user_id: props.userId})}>Delete</button>
+    </div>
 </>
 
 
