fc379c0e0846632ba5f59e4b1ccd46343f7a3f94
tcrurup <tcrurup@gmail.com>
2020-10-09 14:43:13 -0600

    add three column styling to main page

---
+++ table-top-frontend/src/components/FriendsList/FriendsList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class FriendsList extends Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div class="friends-list">
+
+            </div>
+    )}
+}
+
+export default FriendsList
--- table-top-frontend/src/containers/HomePage.js
+++ table-top-frontend/src/containers/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import GameRoomList from '../components/HomePage/GameRoomList.js'
 import NavBar from './NavBar.js'
+import FriendsList from '../components/FriendsList/FriendsList.js'
 import { connect } from 'react-redux'
 import { loadGame, createGame, deleteGame, focusCard, flipCardToFront } from '../actions/gameActions.js'
 import { logout, deleteUser } from '../actions/userActions.js'
@@ -16,22 +17,27 @@ class HomePage extends Component{
     }
     
     render(){
-        console.log(this.navBarOptions())
         return<>
             <NavBar 
                 options={this.navBarOptions()}
             />
-            <h1>Hello {this.props.user.username} welcome to Table Top</h1>
-            < GameRoomList
-                user={this.props.user} 
-                gameRooms={this.props.user.game_rooms}
-                gamesWithCharacters={this.props.user.games_user_part_of} 
-                loadGame = {this.props.loadGame}
-                createGame = {this.props.createGame}
-                deleteGame = {this.props.deleteGame}
-                focusCard = {this.props.focusCard}
-                flipCardToFront = {this.props.flipCardToFront}    
-            />
+            <div class='column left'></div>
+            <div class='column middle'>
+                <h1>Hello {this.props.user.username} welcome to Table Top</h1>
+                < GameRoomList
+                    user={this.props.user} 
+                    gameRooms={this.props.user.game_rooms}
+                    gamesWithCharacters={this.props.user.games_user_part_of} 
+                    loadGame = {this.props.loadGame}
+                    createGame = {this.props.createGame}
+                    deleteGame = {this.props.deleteGame}
+                    focusCard = {this.props.focusCard}
+                    flipCardToFront = {this.props.flipCardToFront}    
+                />
+            </div>
+            <div class="column right">
+                < FriendsList />
+            </div>
         </> 
     }
 }
